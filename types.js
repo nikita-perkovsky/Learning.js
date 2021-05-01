dc2bf0a10f8a9f390b0084629fe688d6075aad7e
Nikita Perkovsky <nikita.perkovsky@gmail.com>
2021-05-01 12:09:03 +0300

    Add types

---
+++ types.js
@@ -0,0 +1,30 @@
+// Number 
+// String
+// Boolean
+// Object
+// Null
+// Undefined
+
+var marryAge = 23
+var msg = 'Simple text message'
+var isDoorOpen = true
+var user = {
+    name: 'John',
+    age: 31,
+    hasAccess: false
+}
+var admin = {
+    name: 'Marry',
+    age: marryAge,
+    hasAccess: true
+}
+
+console.log(msg, isDoorOpen)
+console.log('Age: ' + marryAge)
+console.log(user)
+console.log(admin)
+
+console.log(typeof msg)
+console.log(typeof marryAge)
+console.log(typeof isDoorOpen)
+console.log(typeof user)
