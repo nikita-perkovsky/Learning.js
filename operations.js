5cd257f5e31278949be0760bf55b08d5cc0cb67d
Nikita Perkovsky <nikita.perkovsky@gmail.com>
2021-05-01 12:46:50 +0300

    Add operations

---
+++ operations.js
@@ -0,0 +1,16 @@
+var x = 12
+var y = 8
+var one = 1
+var two = 2
+var s1 = 'Hello'
+var s2 = 'World'
+var sTwo = '2'
+
+console.log(x + ' + ' + y + ' =', x + y)
+console.log(x + ' - ' + y + ' = '+ (x - y))
+console.log(x + ' * ' + y + ' =', x * y)
+console.log(x + ' / ' + y + ' =', x / y)
+console.log(one + ' + "' + sTwo + '" =', one + sTwo)
+console.log('3 + 4 + "1" =', 3 + 4 + '1')
+console.log('true + 1 =', true + 1)
+console.log('false + 1 =', false + 1)
