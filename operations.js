ef556f13f6887c94aabb87bc6e445a432b348bc6
Nikita Perkovsky <nikita.perkovsky@gmail.com>
2021-05-03 15:45:48 +0300

    Add additional operations

--- operations.js
+++ operations.js
@@ -14,3 +14,26 @@ console.log(one + ' + "' + sTwo + '" =', one + sTwo)
 console.log('3 + 4 + "1" =', 3 + 4 + '1')
 console.log('true + 1 =', true + 1)
 console.log('false + 1 =', false + 1)
+
+console.log('5 % 2 =', 5 % 2)
+console.log('7 % 2 =', 7 % 2)
+console.log('35 % 7 =', 35 % 7)
+console.log('10 % 5 =', 10 % 5)
+console.log('8 % 4 =', 8 % 4)
+
+var i = 1
+i += 3 // i = i + 3
+i -= 3 // i = i - 3
+i *= 3 // i = i * 3
+i /= 3 // i = i / 3
+
+var j = 1
+j++ // j = j + 1
+j-- // j = j - 1
+console.log(j++) // 1
+console.log(j) // 2
+j = 1
+++j // j = j + 1
+--j // j = j - 1
+console.log(++j) // 2
+console.log(j) // 2
