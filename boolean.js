b8a3472577faff5ef0d1c4c04f16368d2032ff4e
Nikita Perkovsky <nikita.perkovsky@gmail.com>
2021-05-04 12:43:50 +0300

    Add boolean

---
+++ boolean.js
@@ -0,0 +1,24 @@
+console.log('5 > 2', 5 > 2)         // true
+console.log('5 < 2', 5 < 2)         // false
+console.log('5 >= 2', 5 >= 2)       // true
+console.log('5 <= 2', 5 <= 2)       // false
+console.log('5 == 5', 5 == 5)       // true
+console.log('5 === 5', 5 === 5)     // true
+console.log('5 == "5"', 5 == "5")   // true
+console.log('5 === "5', 5 === "5")  // false
+console.log('5 != 5', 5 != 5)       // false
+console.log('5 !== 6', 5 !== 6)     // true
+console.log('5 !== "6"', 5 !== "6") // true
+console.log('5 !== 5', 5 !== 5)     // false
+console.log('5 !== "5"', 5 !== "5") // true
+
+console.log(true && true)   // true
+console.log(true && false)  // false
+console.log(false && true)  // false
+console.log(false && false) // false
+console.log(true || true)   // true
+console.log(true && false)  // true
+console.log(false && true)  // true
+console.log(false && false) // false
+console.log(!true)          // false
+console.log(!false)         // true
