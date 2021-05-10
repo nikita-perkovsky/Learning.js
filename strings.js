4c749409a4bc13fc01a0aad1836388bd0ed38590
Nikita Perkovsky <nikita.perkovsky@gmail.com>
2021-05-10 13:27:52 +0300

    Add strings

---
+++ strings.js
@@ -0,0 +1,20 @@
+var message = 'This is simple text'
+
+console.log(message.length)
+console.log(message.toUpperCase())
+
+console.log(message[5])
+console.log(message.charAt(5))
+console.log(message.substr(5, 1))
+
+console.log(message.substr(8, 6))
+console.log(message.indexOf('is'))
+console.log(message.indexOf('cat'))
+
+console.log('My name is "Nick"')
+console.log("My name is 'Nick'")
+console.log('My name is \'Nick\'')
+console.log("My name is \"Nick\"")
+
+var q = 'c:\\temp\\dump\\'
+console.log(q)
